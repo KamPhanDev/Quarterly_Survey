9d55ec8d9f1cd95fa0f55def7d11d3f33cb2b90b
apha11 <kamtorn.phanwises@uksbs.co.uk>
2024-08-06 08:45:41 +0100

    adjust: input text

--- spaceKeywordNeuralNet.py
+++ spaceKeywordNeuralNet.py
@@ -8,9 +8,7 @@ nlp = spacy.load('en_core_web_sm')
 nlp.add_pipe("yake")
 
 # Input text
-doc = nlp("Natural language processing (NLP) is a subfield of linguistics, computer science, and artificial intelligence \
-concerned with the interactions between computers and human language, in particular how to program computers \
-to process and analyze large amounts of natural language data.")
+doc = nlp("Natural language processing (NLP) is a subfield of linguistics, computer science, and artificial intelligence concerned with the interactions between computers and human language, in particular how to program computers to process and analyze large amounts of natural language data.")
 
 # Extract keywords
 for keyword, score in doc._.extract_keywords(n=3):
